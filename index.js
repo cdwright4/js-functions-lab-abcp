9c3a37eeb6b7d1ddd29f58ced8541fb52194f440
Crystal <crystal_wright@yahoo.com>
2018-01-13 22:03:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 function happyHolidays (){
   return "Happy holidays!";
 }
+
 function happyHolidaysTo(firstName) {
-  return `Happy holidays, ${firstName}!`
+  return `Happy holidays, ${firstName}!`;
+}
+
+function happyHolidaysTo(holiday,name) {
+  return 'Happy ${holiday}, ${name}!';
 }
